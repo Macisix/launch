22130d8e9ac09dedae0d62e7f577bf33238583cc
Saifullah Kaka <saifullah@wiztech>
2021-05-22 16:14:45 +0500

    First Commit with Launch page

---
+++ app/Http/Controllers/MailChimpController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use MailchimpTransactional;
+
+class MailChimpController extends Controller
+{
+    //Customer Template Function
+    public function sendMailToClient($template_name, $email)
+    {
+        $message = [
+            "from_name" => env('MAIL_FROM_NAME'),
+            "from_email" => env('MAIL_FROM_ADDRESS'),
+            "subject" => "You’re on your way...  Haulmate",
+            "to" => [
+                [
+                    "email" => $email,
+                    "type" => "to"
+                ]
+            ],
+            "global_merge_vars" => [
+                [
+                    "name" => "First_Name",
+                    "content" => ''
+                ],
+            ],
+            "tags" => [
+                'mail-to-customer',
+            ],
+        ];
+
+        $template_content = array(
+            array(
+                'name' => 'main',
+                'content' => ''),
+        );
+        try {
+            $mailchimp = new MailchimpTransactional\ApiClient();
+            $mailchimp->setApiKey(env('MAILCHIMP_APIKEY'));
+            $response = $mailchimp->messages->sendTemplate(["template_name" => $template_name, "template_content" => $template_content, "message" => $message]);
+            $status = $response[0]->status;
+        } catch (Error $e) {
+            $status = $e->getMessage();
+        }
+        return $status;
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ url('assets/images/favicon.png') }}">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="{{ url('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+    <title>Haulmate</title>
+</head>
+<body>
+<section class="main-content">
+    <div class="container">
+        <div class="row d-sm-none">
+            <div class="col-12 px-0">
+                <img class="img-fluid mobile-img w-100" src="{{ url('assets/images/banner-mb.png') }}" alt="banner">
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-xl-5 d-flex align-items-end">
+                <div class="mb-4">
+                    <img class="mb-3" src="{{ url('assets/images/logo.png') }}" alt="logo">
+                    <h1>Make your move</h1>
+                    <p class="text">We’re launching in Singapore soon. Enter your email address and get 10% off your
+                        move.</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-5">
+                <div class="card">
+                    <div class="card-body">
+                        @if(session()->has('error'))
+                            <div class="alert alert-danger">
+                                {{ session()->get('error') }}
+                            </div>
+                        @endif
+                        @if(session()->has('message'))
+                            <div class="alert alert-success">
+                                {{ session()->get('message') }}
+                            </div>
+                        @endif
+                        <form action="{{url('store')}}" method="post">
+                            @csrf
+                            <h4>A sneak peak into what you can expect</h4>
+                            <div class="media align-items-center mb-3">
+                                <img src="{{ url('assets/images/icon-1.pn') }}g" class="mr-3" alt="...">
+                                <div class="media-body">
+                                    <p class="mb-0">Select from Singapore most trusted moving services</p>
+                                </div>
+                            </div>
+                            <div class="media align-items-center">
+                                <img src="{{ url('assets/images/icon-2.png') }}" class="mr-3" alt="...">
+                                <div class="media-body">
+                                    <p class="mb-0">Services include movers, end of lease cleaning, moving boxes and
+                                        more</p>
+                                </div>
+                            </div>
+
+                            <hr/>
+                            <h4>Be the first to know once we launch in June and get 10% off your move</h4>
+                            <div class="form-group">
+                                <label>Enter your email to get notified when we lanuch</label>
+                                <input type="email" name="email" class="form-control" placeholder="Enter your email here">
+                            </div>
+                            <button type="submit" class="btn btn-secondary float-right mt-1">Submit</button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Javascript -->
+<script src="{{ url('assets/js/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ url('assets/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
