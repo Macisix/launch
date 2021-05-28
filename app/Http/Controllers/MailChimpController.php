015cf36a86e6aa3cc14870191b83132976a0efbe
Saifullah Kaka <saifullah@wiztech>
2021-05-28 18:18:07 +0500

    Mailchimp debug

--- app/Http/Controllers/MailChimpController.php
+++ app/Http/Controllers/MailChimpController.php
@@ -42,11 +42,13 @@ class MailChimpController extends Controller
             $mailchimp = new MailchimpTransactional\ApiClient();
             $mailchimp->setApiKey(env('MAILCHIMP_APIKEY'));
             $resp = $mailchimp->messages->sendTemplate(["template_name" => $template_name, "template_content" => $template_content, "message" => $message]);
-die('Cal232l');
+            echo '<pre>';print_r($resp );echo '</pre>';
         } catch (Error $e) {
-            $resp = $e->getMessage();
+            $resp2 = $e->getMessage();
+            echo '2';
+            echo '<pre>';print_r($resp );echo '</pre>';
         }
-        echo '<pre>';print_r($resp );echo '</pre>';
+
 die('Call');
     }
 }
