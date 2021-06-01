b6c62be5ce9f4af078dd852aa3a2869b972ea14e
Saifullah Kaka <saifullah@wiztech>
2021-06-01 16:54:36 +0500

    Mailchimp changes

--- app/Http/Controllers/MailChimpController.php
+++ app/Http/Controllers/MailChimpController.php
@@ -28,17 +28,27 @@ class MailChimpController extends Controller
         $template_content = array(
             array(
                 'name' => 'main',
-                'content' => 'Test'
+                'content' => ''
             ),
         );
 
         try {
             $status = '';
+            //$mailchimp = new MailchimpTransactional\ApiClient();
+            //$mailchimp->setApiKey(env('MAILCHIMP_APIKEY'));
+            //$resp = $mailchimp->messages->sendTemplate(["template_name" => $template_name, "template_content" => $template_content, "message" => $message]);
+
             $mailchimp = new MailchimpTransactional\ApiClient();
             $mailchimp->setApiKey(env('MAILCHIMP_APIKEY'));
-            $resp = $mailchimp->messages->sendTemplate(["template_name" => $template_name, "template_content" => $template_content, "message" => $message]);
+            $response = $mailchimp->messages->sendTemplate([
+                "template_name" => $template_name,
+                "template_content" => $template_content,
+                "message" => $message,
+            ]);
+            echo '<pre>';print_r($response );echo '</pre>';
         } catch (Error $e) {
             $resp = $e->getMessage();
         }
+        die('Call');
     }
 }
